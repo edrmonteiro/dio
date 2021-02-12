fabd9a4b8bb22e16cf825bdf1bc1e20d45b06136
eduardo <eduardormonteiro@gmail.com>
2021-02-11 21:58:46 -0300

    first day learning javascript

--- javascript/javascript/js/main.js
+++ javascript/javascript/js/main.js
@@ -1,67 +1,10 @@
-const users = ["Guilherme", "Pedro", "Jennifer"];
+function Conta() {}
+Conta.prototype.rendimento = 0;
+Conta.prototype.depositar = function() {}
+Conta.prototype.retirar = function() {}
+Conta.prototype.exibirSaldo = function() {
+  return `O saldo da conta é: ${this.saldo}.`;
+}
 
-const gender = {
-  MAN: Symbol("M"),
-  WOMAN: Symbol("W")
-};
-
-const persons = [
-  {
-    name: "Guilherme",
-    age: 26,
-    gender: gender.MAN
-  },
-  {
-    name: "Pedro",
-    age: 43,
-    gender: gender.MAN
-  },
-  {
-    name: "Jennifer",
-    age: 18,
-    gender: gender.WOMAN
-  }
-];
-
-// Retornar a quantidade de itens de um array
-console.log("Itens: ", persons.length);
-
-// Verificar se é array
-console.log("A variável persons é um array:", Array.isArray(persons));
-
-// Iterar os itens do array
-persons.forEach(person => {
-  console.log(`Nome: ${person.name}`);
-});
-
-// Filtrar array
-const mens = persons.filter(person => person.gender === gender.MAN);
-console.log("\nNova lista apenas com homens:", mens);
-
-// Retornar um novo
-const personsWithCourse = persons.map(person => {
-  person.course = "Introdução ao Javascript";
-  return person;
-});
-
-console.log("\nPessoas com a adição do course:", personsWithCourse);
-
-// Transformar um array em outro tipo
-const totalAge = persons.reduce((age, person) => {
-  age += person.age;
-  return age;
-}, 0);
-
-console.log("\nSoma de idade das pessoas", totalAge);
-
-// Juntando operações
-const totalEvenAges = persons
-  .filter(person => person.age % 2 === 0)
-  .reduce((age, person) => {
-    age += person.age;
-    return age;
-  }, 0);
-console.log(
-  "\nSoma de idades das pessoas que possuem idade par",
-  totalEvenAges
-);
+var lista  = [nome: "rosa" , "cravo" , "tulipa", "margarida"]
+console.log(lista[2]);
